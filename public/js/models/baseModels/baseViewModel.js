405705aa65d88f9c632cb640b4bf1bd9bc4edcca
Piet van Zoen <piet@cozy.co>
2014-07-31 22:18:13 -0700

    update front end things

--- public/js/models/baseModels/baseViewModel.js
+++ public/js/models/baseModels/baseViewModel.js
@@ -5,40 +5,61 @@
  */
 define(['lodash', 'models/baseModels/base'], function (_, Base) {
 
-	/**
-	* Model variables
-	*/
-	var BaseViewModel = function (options) {
+  /**
+  * Model variables
+  */
+  var BaseViewModel = function (options) {
 
-		this.variable = 'value';
+    this.uriSegments = [];
 
-		/**
-		 * Base stuff
-		 */
-		Base.apply(this, arguments);
-		this.baseViewModelInitialize.call(this, options);
+    /**
+     * Base stuff
+     */
+    Base.apply(this, arguments);
+    this.baseViewModelInitialize.call(this, options);
 
-	};
+  };
 
-	/**
-	* Model methods
-	*/
-	_.extend(BaseViewModel.prototype, Base.prototype, {
+  /**
+  * Model methods
+  */
+  _.extend(BaseViewModel.prototype, Base.prototype, {
 
-		baseViewModelInitialize: function (options) {
+    baseViewModelInitialize: function (options) {
 
-			/* add baseViewModel initialize stuff here */
+      this.updateHashUri();
 
-			// fire initialize
-			this.initialize.call(this, options);
-		},
+      // fire initialize
+      this.initialize.call(this, options);
+    },
 
-		initialize: function () {
-			/* this gets overridden by page specific viewModel */
-		}
+    initialize: function () {
+      /* this gets overridden by page specific viewModel */
+    },
 
-	});
+    updateHashUri: function () {
+      var self = this;
 
-	return BaseViewModel;
+      // set params property
+      var update = function () {
+
+        var hash = window.location.hash.replace(/#\/?/, '');
+        self.uriSegments(hash.split('/'));
+
+      };
+
+      // run on first call
+      update();
+
+      // bind to onpopstate on first call
+      window.onpopstate = update;
+
+      // subsiquent calls only run update
+      this.updateHashUri = update;
+    }
+
+  });
+
+  return BaseViewModel;
 
 });
