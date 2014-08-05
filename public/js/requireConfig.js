65121d9f308a3620efd44325ac1c8fa2fd8bcd5c
Piet van Zoen <piet@cozy.co>
2014-08-04 21:33:04 -0700

    fix prod app loading

--- public/js/requireConfig.js
+++ public/js/requireConfig.js
@@ -1,11 +1,12 @@
 requirejs.config({
-	baseUrl: '/js',
-	paths: {
-		ko: 'libs/knockout',
+  baseUrl: '/js',
+  paths: {
+    app: 'app',
+    ko: 'libs/knockout',
     net: 'libs/net',
-		moment: 'libs/moment',
-		lodash: "libs/lodash.custom.min",
-		baseModel: 'models/baseModels/baseModel',
-		baseViewModel: 'models/baseModels/baseViewModel'
-	}
+    moment: 'libs/moment',
+    lodash: "libs/lodash.custom.min",
+    baseModel: 'models/baseModels/baseModel',
+    baseViewModel: 'models/baseModels/baseViewModel'
+  }
 });
